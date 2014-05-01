0c6f009543501ab5c5a88db599c7ba818c8559fb
Capwell <capwed@usxxpoddap1m1.corp.emc.com>
2014-04-30 21:17:16 -0700

    Updates

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+var path = require("path");
+
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-gitbook');
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.initConfig({
+        'gitbook': {
+            development: {
+                input: "./",
+                format: "site",
+                title: "Scala Tour",
+                description: "My location for adding scala findings",
+                github: "dcapwell/scala-tour"
+            }
+        },
+        'gh-pages': {
+            options: {
+                base: '_book'
+            },
+            src: ['**']
+        },
+        'clean': {
+            files: '.grunt'
+        }
+    });
+
+    grunt.registerTask('publish', [
+        'clean',
+        'gitbook',
+        'gh-pages'
+    ]);
+    grunt.registerTask('default', 'gitbook');
+};
